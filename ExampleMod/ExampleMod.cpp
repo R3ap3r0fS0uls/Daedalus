d5aecec16921d948e8981b42b7fc05e48a7d6911
R3ap3r0fS0uls <113536641+R3ap3r0fS0uls@users.noreply.github.com>
2022-10-01 14:42:49 -0500

    Update ExampleMod.cpp
    Fixed problem where ProcessFucntion wasn't being called.

--- ExampleMod/ExampleMod.cpp
+++ ExampleMod/ExampleMod.cpp
@@ -1,5 +1,14 @@
 #include "ExampleMod.h"
+#include "SDK.hpp"
 #include "Utilities/MinHook.h"
+#include "Ue4.hpp"
+#include <filesystem>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <shlobj_core.h>
+#include <direct.h>
+#include <json/json.h>
 
 BPFUNCTION(WriteToFile)
 {
@@ -13,15 +22,21 @@ BPFUNCTION(WriteToFile)
 	stack->SetOutput<bool>("ReturnValue", true);
 }
 
+void LogOther(int32_t value)
+{
+	Log::Print(std::to_string(value));
+}
+
 // Only Called Once, if you need to hook shit, declare some global non changing values
 void ExampleMod::InitializeMod()
 {
+
 	UE4::InitSDK();
 	SetupHooks();
 
 	REGISTER_FUNCTION(WriteToFile);
 
-	//MinHook::Init(); //Uncomment if you plan to do hooks
+	MinHook::Init(); //Uncomment if you plan to do hooks
 
 	//UseMenuButton = true; // Allows Mod Loader To Show Button
 }
@@ -30,11 +45,51 @@ void ExampleMod::InitGameState()
 {
 }
 
-void ExampleMod::BeginPlay(UE4::AActor* Actor)
+template<typename T>
+static T* FindObject(const std::string& name)
 {
+	for (int i = 0; i < UE4::UObject::GObjects->GetAsChunckArray().Num(); ++i)
+	{
+		auto object = UE4::UObject::GObjects->GetAsChunckArray().GetByIndex(i).Object;
+
+		if (object == nullptr)
+		{
+			continue;
+		}
+
+		if (object->GetFullName() == name)
+		{
+			return static_cast<T*>(object);
+		}
+	}
+	return nullptr;
 }
 
 void ExampleMod::ProcessFunction(UE4::UObject* pCallObject, UE4::UFunction* pUFunc)
+{
+}
+
+static UE4::APlayerController* GetPlayerController()
+{
+	static auto fn = FindObject<UE4::UFunction>("Function Engine.GameplayStatics.GetPlayerController");
+	auto GameplayStatics = FindObject<UE4::UGameplayStatics>("Class Engine.GameplayStatics");
+
+	struct
+	{
+		class UE4::UObject* WorldContextObject;
+		int PlayerIndex;
+		class UE4::APlayerController* ReturnValue;
+	}params;
+
+	params.WorldContextObject = UE4::UWorld::GetWorld();
+	params.PlayerIndex = 0;
+
+	GameplayStatics->ProcessEvent(fn, &params);
+
+	return params.ReturnValue;
+}
+
+void ExampleMod::BeginPlay(UE4::AActor* Actor)
 {
 }
 
@@ -48,7 +103,6 @@ void ExampleMod::PostBeginPlay(std::wstring ModActorName, UE4::AActor* Actor)
 		ModActor = Actor;
 	}
 }
-
 void ExampleMod::DX11Present(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, ID3D11RenderTargetView* pRenderTargetView)
 {
 }
@@ -59,4 +113,5 @@ void ExampleMod::OnModMenuButtonPressed()
 
 void ExampleMod::DrawImGui()
 {
+
 }
